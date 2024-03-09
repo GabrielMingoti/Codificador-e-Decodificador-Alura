1d392dc4ad005c32ff0f63cf4a04dc54037a6ada
GabrielMingoti94 <gabrielmingoti1994@gmail.com>
2024-03-09 02:36:58 -0300

    Revert "teste com novo usuário"
    This reverts commit 5707666051dbcd707c23c5d680094e4b43cd5881.

--- interacao/app.js
+++ interacao/app.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 //Teste de alteração no projeto
 //novo usuario Gabriel94Git
 //teste curso git parte 3 aula 1
 // 
+=======
+//Teste de alteração no projeto
+>>>>>>> parent of 5707666 (teste com novo usuário)
